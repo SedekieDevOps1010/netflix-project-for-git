cf49cef122c64a5ca6a45268f4edc7e1ae3c5249
Sedekie Kamara <sedekie05@gmail.com>
2022-09-15 01:30:58 -0500

    python code to loop over numbers. -18

---
+++ test.py
@@ -0,0 +1,34 @@
+# print(2)
+# print(2+2)
+# print("Hello everyone")
+# print("this is our first python class")
+# print ("layee")
+# a = 2
+# if a == 2:
+#     print("a is 2")
+# else:
+#     print("no")
+
+# x = 1
+# if x == 1:
+#     print("No, that's not him")
+# else:
+#     print("yep, that's him")
+
+
+j = 4
+
+while j < 10:
+    print(j)
+    if j != 2:
+        break
+    j +=2
+
+
+
+
+
+
+
+
+
